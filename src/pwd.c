5f6c206db849f0d722833ba8d2596da9eb9b83be
Moys_27 <ana.parres27@gmail.com>
2022-11-17 01:36:43 +0100

    #4 Script pwd.c functional

--- src/pwd.c
+++ src/pwd.c
@@ -1,20 +1,37 @@
 #include <stdio.h>
 #include <unistd.h>
-//#include "conio.h"
+#include <stdlib.h>
 
 
 int main (int argc, char *argv[]){
-    char path[256];
-    // Gestion du numero d'arguments
+    char *path;
     if (argc>2){
         perror("ERROR: Too many parameters \n");
         return 1;
     }
+    if ((argv[1]!= NULL) && (strcmp(argv[1]), "-P")){
+        return pwdP(path);
+    }
+    return pwdL(path);
+}
+
+int pwdP (char *result){
+    char path[256];
     if (getcwd(path,sizeof (path) )== NULL){
-            perror("Something goes wrong with getcwd \n");
-            return 1;
-        }
-        printf("%s \n",path);
-    //getch();
+        perror("Something goes wrong with getcwd \n");
+        return 1;
+    }
+    printf("%s \n",path);
+    result=&path[0];
+    return 0;
+}
+
+int pwdL (char *path){
+    path= getenv("PWD");
+    if (path == NULL){
+        perror("Something goes wrong with getcwd \n");
+        return 1;
+    }
+    printf("%s \n",path);
     return 0;
 }
