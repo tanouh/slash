0d3a870ddb04a0a72eaff0cebf62d3ed3532b51b
Moys_27 <ana.parres27@gmail.com>
2022-11-19 13:23:07 +0100

    #4 Fix pwd.c

--- src/pwd.c
+++ src/pwd.c
@@ -9,10 +9,19 @@ int main (int argc, char *argv[]){
         perror("ERROR: Too many parameters \n");
         return 1;
     }
+    if (argc=0){
+        return pwdL(path);
+    }
+
     if ((argv[1]!= NULL) && (strcmp(argv[1]), "-P")){
         return pwdP(path);
     }
-    return pwdL(path);
+    if ((argv[1]!= NULL) && (strcmp(argv[1]), "-L")) {
+        return pwdL(path);
+    }
+
+    perror("ERROR: Argument not valid \n");
+    return 1;
 }
 
 int pwdP (char *result){
@@ -29,7 +38,7 @@ int pwdP (char *result){
 int pwdL (char *path){
     path= getenv("PWD");
     if (path == NULL){
-        perror("Something goes wrong with getcwd \n");
+        perror("Something goes wrong with getenv \n");
         return 1;
     }
     printf("%s \n",path);
