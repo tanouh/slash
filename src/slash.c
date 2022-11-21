c27ed74af498863bf8ffc9f342d4f5d4f426710d
Moys_27 <ana.parres27@gmail.com>
2022-11-22 00:19:21 +0100

    #7 Prompt prototype

--- src/slash.c
+++ src/slash.c
@@ -3,8 +3,8 @@
 #include <string.h>
 #include <readline/history.h>
 #include <readline/readline.h>
-#include <lexer.c>
-#include <exit.c>
+#include "lexer.c"
+#include "exit.c"
 
 #define MAX_ARGS_NUMBER 4096
 #define MAX_ARGS_STRLEN 4096
@@ -19,10 +19,10 @@
 #define CYAN "\001\033[36m\002"
 #define BASIC "\001\033[00m\002"
 
-int getInput(char *input){
+int getInput(char *input) {
     char *buffer;
-    if ((buffer = fgets(" ", MAX_ARGS_STRLEN, stdin))){
-        while (*buffer == ' ' || *buffer == '\t'){
+    if ((buffer = fgets(" ", MAX_ARGS_STRLEN, stdin))) {
+        while (*buffer == ' ' || *buffer == '\t') {
             buffer++;
         }
         if (strlen(buffer) != 0) {
@@ -33,38 +33,41 @@ int getInput(char *input){
     return 0;
 }
 
+
 static char *initialice_prompt() {
     int valret = 0;
-    printf("hhh");
-    char *prompt = malloc((SIZE_PROMPT + 4 * strlen(GREEN) + 1) * sizeof(prompt));
-    if (prompt = NULL) {
+    char *valret_color = malloc(SIZE_COLOR * sizeof(*valret_color));
+    if (valret) {
+        valret_color = GREEN;
+    } else {
+        valret_color = RED;
+    }
+    char *buff = malloc(( SIZE_VALRET +(SIZE_PROMPT) + 3 * SIZE_COLOR + 1) * sizeof(*buff));
+
+    if (buff == NULL) {
         valret = 1;
         return NULL;
     }
-    char *valret_color = malloc( SIZE_COLOR* sizeof(valret_color));
-    if (valret) {
-        valret_color = RED;
-    } else {
-        valret_color = GREEN;
-    }
     char *pwd = getenv("PWD");
-    if (strlen(pwd) + 8 + (SIZE_VALRET + 1) < SIZE_PROMPT) {
-        sprintf(prompt, "[%s%d]%s%s%s$", valret_color, valret, CYAN, pwd, BASIC);
+    if (strlen(pwd) + (SIZE_VALRET + 1) < SIZE_PROMPT) {
+        sprintf(buff, "[%s%d]%s%s%s$", valret_color, valret, CYAN, pwd, BASIC);
     } else {
         char *reduction = pwd + strlen(pwd) + 8 - SIZE_PROMPT;
-        sprintf(prompt, "[%s%d]%s%s%s%s$", valret_color, valret, CYAN, "...", reduction, BASIC);
+        sprintf(buff, "[%s%d]%s%s%s%s$", valret_color, valret, CYAN, "...", reduction, BASIC);
     }
-    return prompt;
+    return buff;
 }
 
-int main(){
+int main() {
+    //initialice_prompt();
     char *input = (char *) malloc(MAX_ARGS_STRLEN);
-    while(1){
-        //prompt()
-        if(getInput(input)) continue;
+
+    while (1) {
+
+        if (getInput(input)) continue;
         lexer(input);
 
-    free(input);
-    return 0;
+        free(input);
+        return 0;
     }
 }
