cab827dc03abdba17127aa747b85425464e3123f
Lucas Ivan <ivan.lucasreiner@gmail.com>
2022-11-22 16:34:49 +0100

    Ajout lexer.h et exit.h

---
+++ src/exit.h
@@ -0,0 +1,6 @@
+#ifndef PROJET_SY5_EXIT_H
+#define PROJET_SY5_EXIT_H
+
+void exit_slash(int status);
+
+#endif //PROJET_SY5_EXIT_H
---
+++ src/lexer.h
@@ -0,0 +1,7 @@
+#ifndef PROJET_SY5_LEXER_H
+#define PROJET_SY5_LEXER_H
+
+#include "token.h"
+int lex(char *input, struct tokenList *tokList);
+
+#endif //PROJET_SY5_LEXER_H
