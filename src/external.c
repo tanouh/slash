55b94d3d05ba9e1f16bc66c34835dfc45d4c4d32
Moys_27 <ana.parres27@gmail.com>
2022-12-10 22:05:49 +0100

    Prompt reglé pour commandes externes

--- src/external.c
+++ src/external.c
@@ -2,42 +2,47 @@
 #include <sys/wait.h>
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 
 #include "external.h"
 
-char ** formate_args(int argc, char ** argv){
-	char ** arg_list = malloc((argc + 1)*  sizeof(char *));
-	for (int i = 0 ; i < argc ; i++){
-		arg_list[i]=argv[i];
-	}
-	arg_list[argc] = NULL;
-	return arg_list;
+char **formate_args(int argc, char **argv) {
+    char **arg_list = malloc((argc + 1) * sizeof(char *));
+    for (int i = 0; i < argc; i++) {
+        arg_list[i] = argv[i];
+    }
+    arg_list[argc] = NULL;
+    return arg_list;
 }
 
-int exec_external(int argc, char ** argv){
-	if (argc < 0){
-		return 1;
-	}
-	char ** args_list = formate_args(argc,argv);
-	pid_t proc = fork();
+int exec_external(int argc, char **argv) {
+    if (argc < 0) {
+        return 1;
+    }
+    char **args_list = formate_args(argc, argv);
+    pid_t proc = fork();
+    int stat = 0;
+    int retval = 0;
+    switch (proc) {
+        case -1:
+            return 1;
+            break;
+        case 0:
+            dup2(STDOUT_FILENO, STDOUT_FILENO);
 
-	switch(proc){
-		case -1:
-			return 1;
-		case 0:
-			dup2(STDERR_FILENO,STDOUT_FILENO);
-			int status = execvp(argv[0], args_list);
-			if(status == -1) {
+            int status = execvp(argv[0], args_list);
+            if (status == -1) {
 
-				dprintf(STDERR_FILENO, "%s: command not found\n", argv[0]);	
-
-			}
-			break;
-		default:
-			wait(NULL);
-			free(args_list);
-
-	} 
-	return 0;
+                dprintf(STDERR_FILENO, "%s: command not found\n", argv[0]);
+                return 1;
+            }
+        default:
+            waitpid(proc, &stat, 0);
+            if (WIFEXITED(stat)) {
+                retval = WEXITSTATUS(stat);
+            }
+            wait(NULL);
+            free(args_list);
+    }
+    return retval;
 }
